392dfac64760817d3236e4f38b9607406f6c057b
niros <niro655@gmail.com>
2022-08-16 13:08:20 +0300

    Declare winner & looser

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const BOX_SIZE = 90;
 export const BOX_MARGIN = 4;
 export const CARD_SIZE = (BOX_SIZE * 3) + ((BOX_MARGIN * 2) * 4); // Do not change it!
 const colors = ['#F247A0', '#4978F4']
-export const WINNER_COLOR = colors.splice((Math.floor(Math.random() * colors.length)), 1);
-export const LOOSER_COLOR = colors.splice((Math.floor(Math.random() * colors.length)), 1);
+export const WINNER_COLOR = colors[0];
+export const LOOSER_COLOR = colors[1];
